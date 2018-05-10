607e73614a7fab69115e7e31938c5c37d2da14bb
tmayweather <tnea.mayweather@hotmail.com>
2018-05-09 21:57:54 -0400

    add fn news, change user search, add basic styling

--- index.js
+++ index.js
@@ -3,7 +3,7 @@ const app = express();
 const request = require("request");
 const bodyParser = require("body-parser");
 const Fortnite = require("fortnite-api");
-const config = require("./config.js").options;
+// const config = require("./config.js").options;
 const access = require("./config.js").access;
 
 app.use(bodyParser.urlencoded({ extended: false }));
@@ -19,38 +19,71 @@ let fortniteAPI = new Fortnite(access,
 app.get("/", (req, res) => {
   
   fortniteAPI.login().then(() => {
-    fortniteAPI
-      .checkFortniteStatus()
-      .then(status => {
-        console.log(status);
-        res.render("landing", {status: status});
-
-        })
+   var news = fortniteAPI.getFortniteNews("en")
+      var status = fortniteAPI.checkFortniteStatus()
+     Promise.all([news, status]) 
+      .then(([news, status]) => {
+        console.log(news);
+       news = news.br
+       res.render("landing", {news: news, status: status});
+      })
       .catch(err => {
         console.log(err);
       });
-  });
+      
+    });
 })
 
+
+// app.get("/stats", (req, res) => {
+//   var user = req.query.user;
+//   var platform = req.query.platform;
+//   var options = {
+//     url: "https://api.fortnitetracker.com/v1/profile/",
+//    };
+// request(options, (error, response, body) => {
+//     if (!error && response.statusCode == 200) {
+      
+//       const playerInfo = JSON.parse(body);
+//       const playerStat = playerInfo.lifeTimeStats
+      
+//       console.log(playerInfo);
+      
+//       res.render("index", {playerStat: playerStat, playerInfo: playerInfo});
+//      } else {
+//       console.log(error)
+//      }
+// });
+// });
+
+
 app.get("/stats", (req, res) => {
-  var user = req.query.user;
-  var platform = req.query.platform;
-  var options = {
-    url: "https://api.fortnitetracker.com/v1/profile/" + platform + "/" + user,
-   };
-request(options, (error, response, body) => {
-    if (!error && response.statusCode == 200) {
-      const playerInfo = JSON.parse(body);
-      const playerStat = playerInfo.lifeTimeStats
-      
-      console.log(playerInfo)
-      
-      res.render("index", {playerStat: playerStat, playerInfo: playerInfo});
-     } else {
-      console.log(error)
-     }
+  const user = req.query.user;
+  const platform = req.query.platform;
+
+  fortniteAPI.login().then(() => {
+  fortniteAPI.getStatsBR(user, platform)
+  .then(stats => {
+    let lifetimeStats = stats.lifetimeStats;
+    let info = stats.info;
+    let solo = stats.group.solo
+    let duo = stats.group.duo
+    let squad = stats.group.squad
+   res.render("index", { 
+     lifetimeStats: lifetimeStats,
+     info: info,
+     solo: solo,
+     duo: duo,
+     squad: squad
+    })
+  })
+  .catch(err => {
+    console.log(err);
+  });
 });
 });
+
+
 
 app.listen(3000, function(){
     console.log('Serving on port 3000!!!')
